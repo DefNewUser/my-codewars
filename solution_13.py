50665a5f3a0ff07c40da0de1cc6539a1ac6b32d4
DefNewUser <def_new_acc@protonmail.com>
2021-11-17 00:47:05 +0300

    Update solution_13

--- solution_13.py
+++ solution_13.py
@@ -15,4 +15,25 @@ same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )
 """
 
 
+def same_structure_as(original, other):
+    if type(original) != type(other):
+        return False
+    if len(original) == len(other) == 0:
+        return True
+    if len(original) != len(other):
+        return False
+    for i in range(len(original)):
+        if bool(type(original[i]) == list) != bool(type(other[i]) == list):
+            return False
+        elif type(original[i]) == type(other[i]) == list:
+            return same_structure_as(original[i], other[i])
+    return True
 
+
+def main():
+    print(same_structure_as([1, 1, 1], [2, 2, 2]))
+    # print(same_structure_as([1, [1, 1]], [[2, 2], 2]))
+
+
+if __name__ == '__main__':
+    main()
